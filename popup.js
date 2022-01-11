1fd803bb9a4c00b97a9a87b9b8a43613391af222
Shivam Pakade <64470228+Shivam-alt-sketch@users.noreply.github.com>
2022-01-11 02:26:32 -0500

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector(".modal");
+const btn = document.querySelector(".btn");
+const close = document.querySelector(".close");
+
+btn.addEventListener("click",openModal);
+bun.addEventListener("click",openModal);
+close.addEventListener("click",closeModal);
+modal.addEventListener("click",closeModal);
+
+
+function openModal(e) {
+    e.preventDefault();
+    modal.style.display = "block";
+}
+
+function closeModal(e) {
+    e.preventDefault();
+    modal.style.display = "none";
+}
